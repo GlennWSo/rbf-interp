2c3f997c64717a6d252df8e176901ffe4f531e14
gws <glennws@pm.me>
2024-10-03 15:08:20 +0200

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 //! A library for multidimensional interpolation.
 
+#![allow(unused)]
+
+use std::fmt::Debug;
 use std::{marker::PhantomData, ops::Neg, time::Instant};
 
 pub use nalgebra as na;
@@ -43,7 +46,7 @@ is that we have one parameter to tune - R0. Too small R0 will make model sharp a
 non-smooth, and too large one will result in system with ill-conditioned matrix.
 Gaussian(f32),
 **/
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct ThinPlateSpline;
 impl BasisFunction for ThinPlateSpline {
     fn eval(v: f32) -> f32 {
@@ -54,7 +57,7 @@ impl BasisFunction for ThinPlateSpline {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Gaussian<const R1000: u32>;
 impl<const R1000: u32> BasisFunction for Gaussian<R1000> {
     const R: f32 = R1000 as f32 / 1000 as f32;
@@ -72,22 +75,16 @@ pub type Vec3 = na::MatrixMN<f32, na::Dynamic, na::U3>;
 pub type Vec2 = na::MatrixMN<f32, na::Dynamic, na::U2>;
 pub type Vec1 = na::MatrixMN<f32, na::Dynamic, na::U1>;
 pub type Row3 = na::RowVector3<f32>;
+pub type Row2 = na::RowVector2<f32>;
 pub type XY = na::Matrix1x2<f32>;
 
 #[derive(Clone, Debug)]
 pub struct Scatter<B: BasisFunction> {
-    /// define basis function trough generics for static performance
     phantom_basis: PhantomData<B>,
-    // TODO(explore): use matrix & slicing instead (fewer allocs).
-    // An array of n vectors each of size m.
-    // centers: Vec2,
-    // An m x n' matrix, where n' is the number of basis functions (including polynomial),
-    // and m is the number of coords.
-    // deltas: Vec1,
     data: Vec3,
 }
 
-type column =
+type Column =
     nalgebra::Matrix<f32, Dynamic, U1, SliceStorage<'static, f32, Dynamic, U1, U1, Dynamic>>;
 
 impl<B: BasisFunction> Scatter<B> {
@@ -105,43 +102,27 @@ impl<B: BasisFunction> Scatter<B> {
         res
     }
 
-    // The order for the polynomial part, meaning terms up to (order - 1) are included.
-    // This usage is consistent with Wilna du Toit's masters thesis "Radial Basis
-    // Function Interpolation"
     pub fn create(
         // centers: Vec2,
         // vals: Vec1,
         samples: Vec3,
         // order: usize,
     ) -> Scatter<B> {
-        dbg!("creating rbf");
         let mut data = samples;
-        let n = dbg!(data.nrows());
-        // n x m matrix. There's probably a better way to do this, ah well.
-        // let mut vals = DMatrix::from_columns(&vals).transpose();
-        // We translate the system to center the mean at the origin so that when
-        // the system is degenerate, the pseudoinverse below minimizes the linear
-        // coefficients.
-        // let means: Vec<_> = Vec::new();
-        // let vals_mat = DMatrix::f(&[vals]);
+        let n = data.nrows();
         let centers = data.columns_range(..2);
         let mat = DMatrix::from_fn(n, n, |r, c| {
             B::eval((centers.row(r) - centers.row(c)).norm())
         });
         let n_non_zero = mat.iter().filter(|e| **e != 0.0).count() as f32;
-        let len = dbg!(mat.len()) as f32;
-        dbg!(n_non_zero / len);
-        // let coo = na::sp
+        let len = mat.len() as f32;
 
-        dbg!("solving rbf deltas");
         let rbf_solve_time = Instant::now();
         let svd = SVD::new(mat.transpose(), true, true);
         let eps = 1e-12;
         let vals = data.column(2);
         let deltas = svd.solve(&vals, eps).unwrap();
         data.set_column(2, &deltas);
-        dbg!(rbf_solve_time.elapsed());
-        dbg!("rbf created");
         let scatter = Scatter {
             phantom_basis: PhantomData,
             data,
