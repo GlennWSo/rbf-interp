07f112ed05626de65c0d7fafa7341a694105ea06
gws <glennws@pm.me>
2024-10-28 13:53:14 +0100

    nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Build a cargo project without extra checks";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      # Common arguments can be set here to avoid repeating them later
+      # Note: changes here will rebuild all dependency crates
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          rust-analyzer
+        ];
+      };
+    });
+}
