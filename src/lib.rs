ca20378099d1a8b4741df9c4e65576f4430691a0
gws <glennws@pm.me>
2025-04-08 11:59:01 +0200

    changed signatures to more generic/std types

--- src/lib.rs
+++ src/lib.rs
@@ -8,28 +8,8 @@ use std::{marker::PhantomData, ops::Neg, time::Instant};
 use serde::{Deserialize, Serialize};
 
 pub use nalgebra as na;
-use nalgebra::{DMatrix, DMatrixSlice, DVector, Dynamic, SliceStorage, SVD, U1};
-
-#[derive(Clone)]
-pub enum BasisKind {
-    /**
-    aka thinplate spline
-    φ(r) = ln(r) * r.pow(N)
-    pros: do not need tuning
-    cons: not compact, ie is allways non zero. >> dense matrix >> less performance
-    **/
-    PolyHarmonic,
-    /**
-    Gaussian basis function: φ(r) = exp(−(ε*r).pow(2)) where ε = 1/R0
-    The main benefit of this function is that it is compact - it is small at distances about 3·R0 from the center. At
-    distances equal to 6·R0 or larger this function can be considered zero. As result,
-    we have to solve linear system with sparse matrix. However, significant drawback
-    is that we have one parameter to tune - R0. Too small R0 will make model sharp and
-    non-smooth, and too large one will result in system with ill-conditioned matrix.
-    Gaussian(f32),
-    **/
-    Gaussian,
-}
+use nalgebra::{DMatrix, DMatrixSlice, DVector, Dynamic, MatrixSlice1x2, SliceStorage, SVD, U1};
+pub type Scatter<T> = Scatter2<T>;
 
 pub trait BasisFunction: 'static + Clone {
     /// a constant parameter for that may be used by the function
@@ -84,38 +64,42 @@ pub type XY = na::Matrix1x2<f32>;
 
 #[derive(Clone, Debug)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-pub struct Scatter<B: BasisFunction> {
+pub struct Scatter2<B: BasisFunction> {
     phantom_basis: PhantomData<B>,
-    data: Vec3,
+    centers: Vec2,
+    deltas: Vec1,
+    // data: Vec3,
 }
 
 type Column =
     nalgebra::Matrix<f32, Dynamic, U1, SliceStorage<'static, f32, Dynamic, U1, U1, Dynamic>>;
 
-impl<B: BasisFunction> Scatter<B> {
-    pub fn eval(&self, coord: XY) -> f32 {
-        let centers = self.data.columns_range(..2);
-        let basis = DVector::from_fn(self.data.nrows(), |row, _c| {
+impl<B: BasisFunction> Scatter2<B> {
+    pub fn eval(&self, coord: &[f32; 2]) -> f32 {
+        let centers = &self.centers;
+        let n = self.deltas.len();
+        let coord = MatrixSlice1x2::from_slice(coord);
+
+        // TODO are you parrallel?
+        let basis = DVector::from_fn(n, |row, _c| {
             let center = centers.row(row);
             let delta = coord - center;
             let r = (delta).norm();
             B::eval(r)
-            // linear component
         });
-        let deltas = self.data.column(2);
 
-        basis.dot(&deltas)
+        basis.dot(&self.deltas)
     }
 
     pub fn create(
         // centers: Vec2,
+        centers: &[[f32; 2]],
+        vals: &[f32],
         // vals: Vec1,
-        samples: Vec3,
-        // order: usize,
-    ) -> Scatter<B> {
-        let mut data = samples;
-        let n = data.nrows();
-        let centers = data.columns_range(..2);
+    ) -> Scatter2<B> {
+        let n = vals.len();
+        let centers: Vec2 = Vec2::from_fn(n, |r, c| centers[r][c]);
+        let vals = Vec1::from_row_slice(vals);
         let mat = DMatrix::from_fn(n, n, |r, c| {
             B::eval((centers.row(r) - centers.row(c)).norm())
         });
@@ -125,13 +109,47 @@ impl<B: BasisFunction> Scatter<B> {
         let rbf_solve_time = Instant::now();
         let svd = SVD::new(mat.transpose(), true, true);
         let eps = 1e-12;
-        let vals = data.column(2);
+        // let vals = data.column(2);
         let deltas = svd.solve(&vals, eps).unwrap();
-        data.set_column(2, &deltas);
+        // data.set_column(2, &deltas);
 
-        Scatter {
+        Scatter2 {
             phantom_basis: PhantomData,
-            data,
+            centers,
+            deltas,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn sinplane() -> (Vec<[f32; 2]>, Vec<f32>) {
+        let mut coords: Vec<_> = Vec::with_capacity(41);
+        let mut vals = Vec::with_capacity(41);
+        for c in -20..=20 {
+            let x = (c as f32) / 5.0;
+            for r in -20..=20 {
+                let y = (r as f32) / 5.0;
+                let r = (x.powi(2) + y.powi(2)).sqrt();
+                let z = r.cos();
+                coords.push([x, y]);
+                vals.push(z);
+            }
+        }
+        (coords, vals)
+    }
+
+    #[test]
+    fn test_scatter2d() {
+        let (coords, vals) = sinplane();
+        let scatter = Scatter2::<ThinPlateSpline>::create(&coords, &vals);
+        for (c, v) in coords.iter().zip(vals) {
+            let h = scatter.eval(c);
+            println!("c: {:?}, v: {}, h: {}", c, v, h);
+            assert!((v - h).abs() < 0.01);
+        }
+        // panic!();
     }
 }
