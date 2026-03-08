532b058628184466e89e78fd8153f2c9112aef1b
gws <glennws@pm.me>
2026-03-08 23:16:20 +0100

    dx: update nix dev env

--- flake.nix
+++ flake.nix
@@ -1,9 +1,12 @@
 {
-  description = "Build a cargo project without extra checks";
+  description = "flake for rust dev";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
+    crane.url = "github:ipetkov/crane";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -11,20 +14,78 @@
     self,
     nixpkgs,
     flake-utils,
+    rust-overlay,
+    crane,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-      # Common arguments can be set here to avoid repeating them later
-      # Note: changes here will rebuild all dependency crates
-    in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          cargo
-          rust-analyzer
-          rustfmt
-          clippy
-        ];
-      };
-    });
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {inherit system overlays;};
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = ["rust-src"];
+          targets = [
+            # "wasm32-unknown-unknown"
+          ];
+        };
+        craneLib = (crane.mkLib pkgs).overrideToolchain (_p: rust);
+
+        commonRust = {
+          src = craneLib.cleanCargoSource ./.;
+          buildInputs = with pkgs; [
+            # Add extra build inputs here, etc.
+            openssl
+            alsa-lib.dev
+            udev.dev
+            xorg.libX11.dev
+            xorg.libXcursor.dev
+            xorg.libXi.dev
+            udev
+
+            clang
+            lld
+          ];
+          nativeBuildInputs = with pkgs; [
+            # Add extra native build inputs here, etc.
+            pkg-config
+          ];
+        };
+
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+          glib
+          gtk3
+          libxkbcommon
+          libz
+          pkg-config
+          vulkan-loader
+          wayland
+          wayland-protocols
+          zlib
+          alsa-lib.dev
+          udev.dev
+          udev
+          alsa-lib
+        ]);
+
+        cargoArtifacts = craneLib.buildDepsOnly (commonRust
+          // {
+            # Be warned that using `//` will not do a deep copy of nested sets
+            pname = "mycrate-deps";
+          });
+      in rec {
+        packages.default = packages.bkad;
+        devShells.default = craneLib.devShell {
+          inherit LD_LIBRARY_PATH;
+          inputsFrom = [packages.bkad];
+          packages = with pkgs; [
+            rust-analyzer
+            bacon
+          ];
+        };
+        packages.bkad = craneLib.buildPackage (commonRust
+          // {
+            inherit cargoArtifacts;
+          });
+      }
+    );
 }
